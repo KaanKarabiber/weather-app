ff8f67d087f140fe76b1f25170285c2d896117d7
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-01-06 20:52:54 +0300

    add asyn function to get weather data from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+const WEATHERAPIKEY = 'GRUJ8YUSWV9C7U29H4QTGHSUX';
+
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul?key=${WEATHERAPIKEY}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error', error);
+  }
+}
+getWeatherData();
