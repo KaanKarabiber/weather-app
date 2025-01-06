76eae864c8d8448f398c5cc225ac4c57aa48d8e0
KaanKarabiber <113041766+KaanKarabiber@users.noreply.github.com>
2025-01-06 14:21:29 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // Add Node.js globals like `require` and `__dirname`
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: prettier.rules,
+  },
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
