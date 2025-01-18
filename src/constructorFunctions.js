86fc5b40da6e1d28ec4e18338a75e6a137dd16c4
Kaan Karabiber <[kaankarabiber1@gmail.com]>
2025-01-18 20:46:39 +0300

    add extract data calcs here

--- src/constructorFunctions.js
+++ src/constructorFunctions.js
@@ -1,9 +1,19 @@
+import {
+  changeIconCamelCase,
+  fahrenheitToCelcius,
+  milesToKm,
+  roundDownValue,
+} from './index';
+
 export function CurrentConditions(
   conditions,
   resolvedAddress,
-  temp,
-  feelsLike,
-  windspeed,
+  tempC,
+  tempF,
+  feelsLikeC,
+  feelsLikeF,
+  windspeedMiles,
+  windspeedKm,
   humidity,
   precipProb,
   description,
@@ -14,38 +24,46 @@ export function CurrentConditions(
 ) {
   this.conditions = conditions;
   this.resolvedAddress = resolvedAddress;
-  this.temp = temp;
-  this.feelsLike = feelsLike;
-  this.windspeed = windspeed;
-  this.humidity = humidity;
-  this.preciProb = precipProb;
+  this.tempC = fahrenheitToCelcius(tempC);
+  this.tempF = roundDownValue(tempF);
+  this.feelsLikeC = fahrenheitToCelcius(feelsLikeC);
+  this.feelsLikeF = roundDownValue(feelsLikeF);
+  this.windspeedMiles = roundDownValue(windspeedMiles);
+  this.windspeedKm = milesToKm(windspeedKm);
+  this.humidity = roundDownValue(humidity);
+  this.preciProb = roundDownValue(precipProb);
   this.description = description;
-  this.sunrise = sunrise;
-  this.sunset = sunset;
+  this.sunrise = sunrise.split(':').slice(0, 2).join(':');
+  this.sunset = sunset.split(':').slice(0, 2).join(':');
   this.dateTime = dateTime;
-  this.icon = icon;
+  this.icon = changeIconCamelCase(icon);
 }
 export function Days(
   date,
   condition,
   precipProb,
-  tempmax,
-  tempmin,
+  tempmaxC,
+  tempmaxF,
+  tempminC,
+  tempminF,
   icon,
   hours = []
 ) {
   this.date = date;
   this.condition = condition;
-  this.preciProb = precipProb;
-  this.tempmax = tempmax;
-  this.tempmin = tempmin;
-  this.icon = icon;
+  this.preciProb = roundDownValue(precipProb);
+  this.tempmaxC = fahrenheitToCelcius(tempmaxC);
+  this.tempmaxF = roundDownValue(tempmaxF);
+  this.tempminC = fahrenheitToCelcius(tempminC);
+  this.tempminF = roundDownValue(tempminF);
+  this.icon = changeIconCamelCase(icon);
   this.hours = hours;
 }
-export function Hours(dateTime, precipProb, temp, condition, icon) {
-  this.dateTime = dateTime;
-  this.preciProb = precipProb;
-  this.temp = temp;
+export function Hours(dateTime, precipProb, tempC, tempF, condition, icon) {
+  this.dateTime = dateTime.split(':').slice(0, 2).join(':');
+  this.preciProb = roundDownValue(precipProb);
+  this.tempC = fahrenheitToCelcius(tempC);
+  this.tempF = roundDownValue(tempF);
   this.condition = condition;
-  this.icon = icon;
+  this.icon = changeIconCamelCase(icon);
 }
